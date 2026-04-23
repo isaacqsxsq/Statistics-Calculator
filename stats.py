1822611649a6e3007d2323b47f65c42a318c6c0e
Isaac <isaacqsxsq@gmail.com>
2026-04-23 22:06:36 +0900

    finished the whole tests and stats functions (now finish the menu)

--- stats.py
+++ stats.py
@@ -7,19 +7,69 @@ def mean(arr):
     return summ / n
 
 def median(arr):
-    pass
-
+    n = len(arr)
+    d = int(n/2)
+    if n % 2 == 0:
+        return (arr[d] + arr[d-1]) / 2
+    else:
+        return arr[d]
+        
 def mode(arr):
-    pass
+    d = {}
+    maxx = 0
+    for num in arr:
+        d[num] = d.get(num, 0) + 1
+        maxx = max(maxx, d[num])
+    
+    ans = []
+    
+    for n, freq in d.items():
+        if freq == maxx:
+            ans.append(n)
+    
+    return ans
+     
 
 def variance(arr):
-    pass
+    m = mean(arr)
+    sol = 0 
+    for num in arr:
+        sol += (num - m) ** 2
+    
+    return sol / (len(arr))
 
 def std(arr):
-    pass
+    return variance(arr) ** 0.5
 
 def correlation(arr1, arr2):
-    pass
+    std1 = std(arr1)
+    std2 = std(arr2)
+    cov = covariance(arr1, arr2)
+    
+    return cov / (std1 * std2)
+
+def covariance(arr1, arr2):
+    mean1 = mean(arr1)
+    mean2 = mean(arr2)
+    
+    sol = 0 
+    for i in range(len(arr1)):
+        sol += (arr1[i] - mean1) * (arr2[i] - mean2)
+    
+    return sol / (len(arr1))
 
 def min_max(arr1):
-    pass
+    min = float('inf')
+    max = float('-inf')
+    
+    for num in arr1:
+        if num < min:
+            min = num
+            
+        if num > max:
+            max = num
+        
+    for i in range(len(arr1)):
+        arr1[i] = round((arr1[i] - min) / (max - min), 5)
+    
+    return arr1
--- tests.py
+++ tests.py
@@ -1,18 +1,22 @@
 from stats import mean, median, mode, variance, std, min_max, correlation
 
 data        = [4, 8, 15, 16, 23, 42]
+data1       = [4, 8, 15, 16, 23]
 data_mode   = [4, 8, 8, 15, 16, 16, 16, 23, 42]
+data_mode2   = [4, 8, 8, 8, 15, 16, 16, 16, 23, 42]
 x           = [4, 8, 15, 16, 23, 42]
 y           = [1, 3, 6, 7, 9, 17]
 
 def is_close(a, b, tol=0.001):
-    return abs(a - b) < tol
+    return abs(a - b) <= tol
 
 print("=== Running Tests ===")
 print(f"mean: {'PASS' if mean(data) == 18.0 else 'FAIL'}")
-# print(f"median:      {'✅ PASS' if median(data) == 15.5 else '❌ FAIL'}")
-# print(f"mode:        {'✅ PASS' if mode(data_mode) == [16] else '❌ FAIL'}")
-# print(f"variance:    {'✅ PASS' if is_close(variance(data), 151.667) else '❌ FAIL'}")
-# print(f"std_dev:     {'✅ PASS' if is_close(std(data), 12.315) else '❌ FAIL'}")
-# print(f"std_dev:     {'✅ PASS' if min_max(data) == [4, 8, 15, 16, 23, 42] else '❌ FAIL'}")
-# print(f"correlation: {'✅ PASS' if is_close(correlation(x, y), 0.9982) else '❌ FAIL'}")
+print(f"median:      {'PASS' if median(data) == 15.5 else 'FAIL'}")
+print(f"median:      {'PASS' if median(data1) == 15.0 else 'FAIL'}")
+print(f"mode:        {'PASS' if mode(data_mode) == [16] else 'FAIL'}")
+print(f"mode:        {'PASS' if mode(data_mode2) == [8, 16] else 'FAIL'}")
+print(f"variance:    {'PASS' if is_close(variance(data), 151.667) else 'FAIL'}")
+print(f"std_dev:     {'PASS' if is_close(std(data), 12.315) else 'FAIL'}")
+print(f"min_max:     {'PASS' if min_max(data) == [0.0, 0.10526, 0.28947, 0.31579, 0.5, 1.0] else 'FAIL'}")
+print(f"correlation: {'PASS' if is_close(correlation(x, y), 0.9982) else 'FAIL'}")
